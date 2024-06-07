9308e2fc3d50debe3020f83453af10b711ee02db
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-06-07 14:59:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import Name from './Name';
+import Price from './Price';
+import Description from './Description';
+import Image from './Image';
 
-function App() {
+const App = () => {
+  const firstName = "Uchenna Hanson";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Name />
+          <Price />
+          <Description />
+          <Image />
+        </Card.Body>
+      </Card>
+      <p>Hello, {firstName ? firstName : "there!"}</p>
+      {firstName && <img src={`https://avatars.githubusercontent.com/u/105205124?v=4${firstName.toLowerCase()}.jpg`} alt={firstName} />}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/product.js
@@ -0,0 +1,10 @@
+// src/product.js
+const product = {
+    name: "Product Name",
+    price: "$10",
+    description: "Product Description",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbbHMLbVwAUDT7B-oqUxYbrZguIHbIckk3yQ&s"
+  };
+  
+  export default product;
+  
